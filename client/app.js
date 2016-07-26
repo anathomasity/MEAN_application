43a75a7f4e7a9f3510276ceebc2df2597d403e96
Ana Thomas <anathomasity@gmail.com>
2016-07-26 12:47:10 -0700

    'changed'

--- client/app.js
+++ client/app.js
@@ -3,15 +3,15 @@ var myApp = angular.module('Myapp', ['ngRoute']);
 (function(){
 	myApp.config(function($routeProvider){
 		$routeProvider
-			.when('/', 
-			{
-				controller: 'profileController',
-				templateUrl: "partials/index.html"
-			})
-			.when('/#section5', {
-				controller: "friendsController", 
-				templateUrl: "partials/friends.html"
-			})
+			// .when('/', 
+			// {
+			// 	controller: 'profileController',
+			// 	templateUrl: "partials/index.html"
+			// })
+			// .when('/#section5', {
+			// 	controller: "friendsController", 
+			// 	templateUrl: "partials/friends.html"
+			// })
 			// .when('/mongoose/:id', 
 			// {
 			// 	controller: 'showController', 
