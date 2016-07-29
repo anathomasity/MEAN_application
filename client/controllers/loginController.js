c2e6967f39fce95193c1088dcb3e09f4ad07d4c5
shivbhardwaj <sbhardwaj192@gmail.com>
2016-07-28 21:09:31 -0700

    changes made to css and other static files

--- client/controllers/loginController.js
+++ client/controllers/loginController.js
@@ -2,10 +2,7 @@ myApp.controller('loginController', function($scope, $location, $cookies, usersF
   // var logged_in_user = $cookies.get('logged_user');
   // console.log(logged_in_user, " this is the logged in user in the loginController");
 
-
-
   $scope.register = function(){
-
     usersFactory.register($scope.registration, function(data){
       if (data.data.errors){
         $scope.errors = data.data.errors;
@@ -18,11 +15,13 @@ myApp.controller('loginController', function($scope, $location, $cookies, usersF
         $cookies.put('logged_user', cookie_userID);
         console.log($cookies.get('logged_user'));
         $location.url('/dashboard');
+        // $scope.register = {};
       }
     }, function(err){
       console.log("I am an error",err);
     })
   }
+
   $scope.login = function(){
     usersFactory.login(
       $scope.userLogin,
